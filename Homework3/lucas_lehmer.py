f6269989be0ace9ade832ff5a976f9394f8a028e
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-04-16 18:59:35 +0300

    Update lucas_lehmer.py
    Something seems wrong

--- Homework3/lucas_lehmer.py
+++ Homework3/lucas_lehmer.py
@@ -1,10 +1,12 @@
 from Homework3 import solovay_strassen
 
 
-def compute_s(stop=1):
+def compute_s(mersen_number, stop=1):
     s = 4
     count = 1
-
+    while count != stop:
+        s = (pow(s, 2) - 2) % mersen_number
+        count += 1
     return s
 
 
@@ -18,7 +20,15 @@ def run(n=None):
     if not solovay_strassen.run(n):
         print("Not Mersen number")
         return False
+    mersen_number = pow(2, n) - 1
+    print("Number:", mersen_number)
+    s = compute_s(mersen_number, n)
+    if s != 0:
+        print("Not Mersen number")
+        return False
+    print("Mersen number")
+    return True
 
 
 if __name__ == '__main__':
-    run()
+    run(7)
